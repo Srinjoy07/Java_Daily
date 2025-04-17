ca1caea4d9120d7a476807f2dfcc662b81519fb1
srinjoy_ganguly <srinjoyganguly2003@gmail.com>
2025-04-17 11:28:53 +0530

    Add files via upload

---
+++ Day-3/Srinu.java
@@ -0,0 +1,26 @@
+class Srinu {
+   public static void main(String[] args) {
+      System.out.println("TASK_1");
+      System.out.println("***");
+      System.out.println("**");
+      System.out.print("*");
+
+      System.out.println();
+      System.out.println();
+
+      System.out.println("TASK_2");
+      System.out.println("Name:Srinjoy Ganguly");
+      System.out.println("Aggregate: 85%");
+      System.out.println("marks: 8.5sgpa");
+      System.out.println("gender:M");
+      System.out.println("phone:+91 9674058734");
+
+      System.out.println();
+      System.out.println();
+
+      System.out.println("TASK_3");
+      System.out.println("Hello,I am Srinjoy");
+      System.out.println("I am a final year student of CSE");
+      System.out.print("I love to watch and play football");    
+   }
+}
